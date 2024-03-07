0eb384ed67c783d2511b8e1f01b393724b0be827
Ameer-Anas99 <ameeranas19041999@gmail.com>
2024-03-08 00:45:13 +0530

    fixing profile page

--- lib/view/profile_page.dart
+++ lib/view/profile_page.dart
@@ -1,6 +1,5 @@
-import 'dart:ffi';
-
 import 'package:chat_app/components/text_box.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -27,58 +26,78 @@ class _ProfilePageState extends State<ProfilePage> {
           child: Text("Profile "),
         ),
       ),
-      body: ListView(
-        children: [
-          const SizedBox(
-            height: 40,
-          ),
-          // profile pic
-          const Icon(
-            Icons.person_2_outlined,
-            size: 100,
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          // user email
-          Text(
-            currentUser.email!,
-            textAlign: TextAlign.center,
-            style: const TextStyle(color: Colors.black, fontSize: 17),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 15),
-            child: Text(
-              "My Details",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-            ),
-          ),
-          // Username
-          TextBoxPage(
-            text: "Ameer Anas",
-            selectionName: "username",
-            onPressed: () => editField("ameer"),
-          ),
-          // Bio
-          TextBoxPage(
-            text: "empty",
-            selectionName: "Bio",
-            onPressed: () => editField("bio"),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 15),
-            child: Text(
-              "My Posts",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-            ),
-          ),
-        ],
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection("Users")
+            .doc(currentUser.email)
+            .snapshots(),
+        builder: (context, snapshot) {
+          // get user data
+          if (snapshot.hasData && snapshot.data!.data() != null) {
+            final userData = snapshot.data!.data()! as Map<String, dynamic>;
+            return ListView(
+              children: [
+                const SizedBox(
+                  height: 40,
+                ),
+                // profile pic
+                const Icon(
+                  Icons.person_2_outlined,
+                  size: 100,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                // user email
+                Text(
+                  currentUser.email!,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.black, fontSize: 17),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 15),
+                  child: Text(
+                    "My Details",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                // Username
+                TextBoxPage(
+                  text: userData['username'] ?? 'No Data Found',
+                  selectionName: 'username',
+                  onPressed: () => editField('username'),
+                ),
+                // Bio
+                TextBoxPage(
+                  text: userData['bio'],
+                  selectionName: 'bio',
+                  onPressed: () => editField('bio'),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 15),
+                  child: Text(
+                    "My Posts",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ],
+            );
+          } else if (snapshot.hasError) {
+            print("a");
+            return Center(
+              child: Text("Erro: r${snapshot.error}"),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
